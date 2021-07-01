372dcb8f9a4a69593559f49ee53e1300fb48d75e
RyanLaiber <ryanlaiber@gmail.com>
2021-06-30 21:14:42 -0300

    adiciona alguns requisitos do exercicio 2, react lifecycle

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ class App extends React.Component {
   constructor() {
     super();
     this.fetchApi = this.fetchApi.bind(this);
+    this.guardaRaca = this.guardaRaca.bind(this);
     this.state = {
       imgPath: '',
       flag: false,
@@ -18,20 +19,37 @@ class App extends React.Component {
     });
     const apiFetch = await fetch('https://dog.ceo/api/breeds/image/random');
     const apiObj = await apiFetch.json();
-    console.log(apiObj.message);
     this.setState({
       imgPath: apiObj.message,
       flag: true,
     });
   }
 
+  guardaRaca() {
+    const { imgPath } = this.state;
+    const race = imgPath.split('/')[4];
+    console.log(race);
+    if (localStorage.dogUrl) {
+      console.log('entrei');
+      localStorage['dogUrl'] = imgPath;
+    } else{
+      localStorage.setItem('dogUrl', imgPath);
+    }
+  }
 
   componentDidMount() {
     this.fetchApi();
   }
 
+  shouldComponentUpdate(nextProps, NextState) {
+    const eleMesmo = NextState.imgPath.includes('terrier');
+    const sim = eleMesmo ? false : true;
+    console.log('confere');
+    return sim;  
+  }
+
   componentDidUpdate() {
-    
+    this.guardaRaca();
   }
 
   render(){
